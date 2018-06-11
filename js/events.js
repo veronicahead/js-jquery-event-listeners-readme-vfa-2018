084672832c64698810b12b780e34f21b7dd5becf
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-11 00:21:49 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -17,7 +17,7 @@ function frameIt() {
 function pressIt() {
   //bind a keydown event to the input field of the form that alers a user when they have pressed the G key
   $('#typing').on("keydown", function() {
-    if ()
+    if ($('#typing').val() === )
   })
 }
 
