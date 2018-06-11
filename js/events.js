d111781ae4d732d0fcdfe620851ef0d0fa8710be
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-11 00:31:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,41 @@
 //define functions here
 
+function getIt() {
+  // bind a click event to p tag, when clicked it alerts "Hey!"
+  $('p').on("click", function() {
+    alert ("Hey!");
+  })
+}
+
+function frameIt() {
+  //bind the load event that adds the class tasty to the image
+  $('img').on("load", function() {
+    $('img').addClass("tasty");
+  })
+}
+
+function pressIt() {
+  //bind a keydown event to the input field of the form that alers a user when they have pressed the G key
+  $('#typing').on("keydown", function(key) {
+    if (key.which == 71) {
+      alert("The G key was pressed.");
+    }
+  })
+}
+
+function submitIt() {
+  //bind a submit event to the form that alerts message
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
