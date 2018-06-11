3bb4f93cdfd65b07b9e7f7ea31241c8c05375729
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-11 00:08:34 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,7 +2,9 @@
 
 function getIt() {
   // bind a click event to p tag, when clicked it alerts "Hey!"
-  $('p').on("click")
+  $('p').on("click", function() {
+    
+  })
 }
 
 $(document).ready(function(){
